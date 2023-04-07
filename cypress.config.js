c222da03ab005a42a8e86dd3cca99e3db927f974
Nick Merwin <n@mer.io>
2023-04-07 09:01:55 -0700

    add retries

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,13 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    "retries": {
+      // Configure retry attempts for `cypress run`
+      // Default is 0
+      "runMode": 2,
+      // Configure retry attempts for `cypress open`
+      // Default is 0
+      "openMode": 0
+    }
   },
 });
